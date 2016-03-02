4dab50714b82bb48e6f6ad2b5c4c12354159cf36
root <delcroix.cedric62@gmail.com>
2016-03-03 00:04:45 +0100

    [TP1] Exo 5 (2)

--- tp2/mon_programme.cpp
+++ tp2/mon_programme.cpp
@@ -4,5 +4,7 @@ using namespace std;
 
 int main(){
 	cout << "Bonjour, mon nom est Alice" << endl;
+	cout << "Alice est dans la classe" << endl;
+	cout << "Sans le moindre doute, Alice travaille sur cet exercice" << endl;
 	return 0;
 }
